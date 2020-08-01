850ce03a290301d5df0c64da3ba77edfd59cb0c5
Chris Badahdah <a.s.badahdah@gmail.com>
2020-08-01 14:28:27 -0700

    Filter text viewer by line

--- src/portal/viewer/text.cljs
+++ src/portal/viewer/text.cljs
@@ -18,23 +18,32 @@
    [s/table
     [s/tbody
      [l/lazy-seq
-      (map-indexed
-       (fn [line line-content]
-         [s/tr
-          {:key line}
-          [s/td
-           {:style
-            {:color (::c/number settings)
-             :font-size (:font-size settings)
-             :user-select :none
-             :text-align :right
-             :padding-right (* 2 (:spacing/padding settings))}}
-           [:span (inc line)]]
-          [s/td
-           {:style
-            {:color (::c/text settings)
-             :font-size (:font-size settings)}}
-           [:pre line-content]]])
-       (str/split value #"\n"))
+      (->>
+       (str/split value #"\n")
+       (map-indexed
+        (fn [line line-content]
+          [(inc line) line-content]))
+       (filter
+        (fn [[_ line-content]]
+          (if-let [search-text (:search-text settings)]
+            (str/includes? line-content search-text)
+            true)))
+       (map
+        (fn [[line line-content]]
+          [s/tr
+           {:key line}
+           [s/td
+            {:style
+             {:color (::c/number settings)
+              :font-size (:font-size settings)
+              :user-select :none
+              :text-align :right
+              :padding-right (* 2 (:spacing/padding settings))}}
+            [:span (inc line)]]
+           [s/td
+            {:style
+             {:color (::c/text settings)
+              :font-size (:font-size settings)}}
+            [:pre line-content]]])))
       {:default-take 100 :step 100}]]]])
 
