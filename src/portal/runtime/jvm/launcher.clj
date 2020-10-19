e8356de226eaae9fc9599bcd7db88fb3ced07016
Nate Jones <nate@endot.org>
2020-10-19 08:17:20 -0700

    Add host and port options for JVM launcher

--- src/portal/runtime/jvm/launcher.clj
+++ src/portal/runtime/jvm/launcher.clj
@@ -35,17 +35,21 @@
    "--no-first-run"
    (str "--app=" url)])
 
-(defn open [options]
-  (swap! rt/state merge {:portal/open? true} options)
+(defn- ensure-server [handler port]
   (when (nil? @server)
     (reset!
      server
-     (http/run-server #'server/handler
-                      {:port 0
+     (http/run-server handler
+                      {:port port
                        :max-ws (* 1024 1024 1024)
-                       :legacy-return-value? false})))
-  (let [session-id (random-uuid)
-        url (str "http://localhost:" (http/server-port @server) "?" session-id)]
+                       :legacy-return-value? false}))))
+
+(defn open [options]
+  (swap! rt/state merge {:portal/open? true} options)
+  (let [{:portal.launcher/keys [port host]} options
+        session-id (random-uuid)
+        _  (ensure-server #'server/handler (or port 0))
+        url (str "http://" (or host "localhost") ":" (http/server-port @server) "?" session-id)]
     (if-let [bin (get-chrome-bin)]
       (let [flags (chrome-flags url)]
         (future (apply sh bin flags)))
