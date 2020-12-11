58a83aa3fbf4892af4fc1400c1b806fd01c7fa01
yuri <yuri@vw-dilab.com>
2020-12-11 17:42:04 +0000

    remove default dependency on yaml

--- src/portal/main.clj
+++ src/portal/main.clj
@@ -1,6 +1,5 @@
 (ns portal.main
   (:require [cheshire.core :as json]
-            [yaml.core :as yaml]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [portal.api :as p]
@@ -10,11 +9,13 @@
 (defn read-edn [reader]
   (with-open [in (PushbackReader. reader)] (edn/read in)))
 
+(defn read-yaml [] (require 'clj-yaml.core) (resolve 'clj-yaml.core/parse-string))
+
 (defn -main [& args]
   (let [[input-format] args
         in (case input-format
              "json"     (-> System/in io/reader (json/parse-stream true))
-             "yaml"     (-> System/in io/reader slurp (yaml/parse-string :keywords true))
+             "yaml"     (-> System/in io/reader slurp ((read-yaml)))
              "edn"      (-> System/in io/reader read-edn)
              "transit"  (-> System/in t/json-stream->edn))]
     (p/open)
