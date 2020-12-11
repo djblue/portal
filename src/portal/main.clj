e7940a70187aa883e2d2504031dade11cba066ec
Yuri Vendruscolo da Silveira <yvendruscolo@gmail.com>
2020-12-11 19:51:12 +0000

    Add YAML support (#27)

--- src/portal/main.clj
+++ src/portal/main.clj
@@ -9,10 +9,13 @@
 (defn read-edn [reader]
   (with-open [in (PushbackReader. reader)] (edn/read in)))
 
+(defn read-yaml [] (require 'clj-yaml.core) (resolve 'clj-yaml.core/parse-string))
+
 (defn -main [& args]
   (let [[input-format] args
         in (case input-format
              "json"     (-> System/in io/reader (json/parse-stream true))
+             "yaml"     (-> System/in io/reader slurp ((read-yaml)))
              "edn"      (-> System/in io/reader read-edn)
              "transit"  (-> System/in t/json-stream->edn))]
     (p/open)
