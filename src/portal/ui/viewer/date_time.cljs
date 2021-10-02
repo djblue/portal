60bd7250f847c09d4e4064d27a913460ae4524b1
Chris Badahdah <a.s.badahdah@gmail.com>
2021-10-02 00:00:21 -0700

    Split apart date-time viewer

--- src/portal/ui/viewer/date_time.cljs
+++ src/portal/ui/viewer/date_time.cljs
@@ -35,12 +35,8 @@
    "November"
    "December"])
 
-(defn inspect-date-time [value]
+(defn inspect-time [value]
   (let [value   (parse value)
-        day     (.getDay value)
-        date    (.getDate value)
-        month   (.getMonth value)
-        year    (.getFullYear value)
         hour    (.getHours value)
         minute  (.getMinutes value)
         second  (.getSeconds value)
@@ -49,50 +45,63 @@
         border  {:color (::c/border theme)}
         keyword {:color (::c/keyword theme)}]
     [s/div
+     {:title "Time"
+      :style
+      {:display :flex
+       :align-items :center}}
+     [s/span
+      {:title "Hour" :style style}
+      (let [hour (mod hour 12)]
+        (if (= hour 0) 12 hour))]
+     [s/span {:style border} ":"]
+     [s/span
+      {:title "Minute" :style style}
+      (when (< minute 10) "0") minute]
+     [s/span {:style border} ":"]
+     [s/span
+      {:title "Second" :style style}
+      (when (< second 10) "0")
+      second]
+     [s/div {:style {:width "0.25em"}}]
+     [s/span
+      {:style keyword}
+      (if (> hour 12) "PM" "AM")]]))
+
+(defn inspect-date [value]
+  (let [value   (parse value)
+        day     (.getDay value)
+        date    (.getDate value)
+        month   (.getMonth value)
+        year    (.getFullYear value)
+        theme   (theme/use-theme)
+        style   {:color (::c/number theme)}
+        border  {:color (::c/border theme)}]
+    [s/div
      {:title "Date"
       :style
       {:display :flex
        :align-items :center}}
-     [s/div
-      {:style
-       {:display :flex
-        :align-items :center}}
-      [s/span
-       {:title (nth months month) :style style}
-       (inc month)]
-      [s/span {:style border} "/"]
-      [s/span
-       {:title (nth days day) :style style}
-       (when (< date 10) "0") date]
-      [s/span {:style border} "/"]
-      [s/span
-       {:title "Year" :style style}
-       year]]
+     [s/span
+      {:title (nth months month) :style style}
+      (inc month)]
+     [s/span {:style border} "/"]
+     [s/span
+      {:title (nth days day) :style style}
+      (when (< date 10) "0") date]
+     [s/span {:style border} "/"]
+     [s/span
+      {:title "Year" :style style}
+      year]]))
 
+(defn inspect-date-time [value]
+  (let [value (parse value)]
+    [s/div
+     {:style
+      {:display :flex
+       :align-items :center}}
+     [inspect-date value]
      [s/div {:style {:width "0.75em"}}]
-
-     [s/div
-      {:title "Time"
-       :style
-       {:display :flex
-        :align-items :center}}
-      [s/span
-       {:title "Hour" :style style}
-       (let [hour (mod hour 12)]
-         (if (= hour 0) 12 hour))]
-      [s/span {:style border} ":"]
-      [s/span
-       {:title "Minute" :style style}
-       (when (< minute 10) "0") minute]
-      [s/span {:style border} ":"]
-      [s/span
-       {:title "Second" :style style}
-       (when (< second 10) "0")
-       second]
-      [s/div {:style {:width "0.25em"}}]
-      [s/span
-       {:style keyword}
-       (if (> hour 12) "PM" "AM")]]]))
+     [inspect-time value]]))
 
 (def viewer
   {:predicate parse
