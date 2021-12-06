e79e205336bdaa2fb7f5132167d42a052776ab9c
Chris Badahdah <a.s.badahdah@gmail.com>
2021-12-05 19:26:19 -0700

    Disable risk dir-local variable for now

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ((nil . ((cider-clojure-cli-global-options     . "-A:dev:cljs:dev-cljs")
-         (cider-custom-cljs-repl-init-form     . "(user/cljs)")
+         ;; (cider-custom-cljs-repl-init-form     . "(user/cljs)")
          (cider-default-cljs-repl              . custom)
          (cider-preferred-build-tool           . clojure-cli)
          (cider-redirect-server-output-to-repl . t)
