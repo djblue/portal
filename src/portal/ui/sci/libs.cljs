d6b83878e6a21bd916a8e1b8001fe04dc9f15677
Chris Badahdah <a.s.badahdah@gmail.com>
2021-12-06 21:05:09 -0700

    Add full ns import into sci

--- src/portal/ui/sci/libs.cljs
+++ src/portal/ui/sci/libs.cljs
@@ -1,54 +1,29 @@
 (ns portal.ui.sci.libs
-  (:require [cljs.reader]
-            [portal.ui.commands]
+  (:require cljs.reader
+            portal.colors
+            portal.ui.inspector
             [portal.ui.sci.import :as sci-import]
-            [portal.ui.state]
-            [reagent.core]
-            [reagent.dom]
+            portal.ui.state
+            portal.ui.styled
+            portal.ui.theme
+            reagent.core
+            reagent.dom
             [sci.core :as sci]))
 
 (def namespaces
-  #_:clj-kondo/ignore
-  (sci-import/import
-   cljs.core/random-uuid
-   cljs.core/tap>
-
-   cljs.reader/read-string
-
-   portal.colors/themes
-   portal.ui.inspector/inspector
-   portal.ui.theme/use-theme
-
-   portal.ui.styled/styled
-   portal.ui.styled/a
-   portal.ui.styled/h1
-   portal.ui.styled/h2
-   portal.ui.styled/h3
-   portal.ui.styled/h4
-   portal.ui.styled/h5
-   portal.ui.styled/h6
-   portal.ui.styled/table
-   portal.ui.styled/table
-   portal.ui.styled/tbody
-   portal.ui.styled/thead
-   portal.ui.styled/tr
-   portal.ui.styled/th
-   portal.ui.styled/td
-   portal.ui.styled/div
-   portal.ui.styled/span
-   portal.ui.styled/input
-   portal.ui.styled/button
-   portal.ui.styled/img
-   portal.ui.styled/iframe
-   portal.ui.styled/select
-   portal.ui.styled/option
-
-   portal.ui.state/invoke
-
-   reagent.core/as-element
-   reagent.core/atom
-
-   reagent.dom/render))
+  (merge
+   (sci-import/import-ns
+    portal.colors
+    portal.ui.inspector
+    portal.ui.state
+    portal.ui.styled
+    portal.ui.theme
+    reagent.core
+    reagent.dom)
+   (sci-import/import
+    cljs.core/random-uuid
+    cljs.core/tap>
+    cljs.reader/read-string)))
 
 (defn init [opts]
   (sci/init
