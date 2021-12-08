e0ae9a78acf3b103746ecb675be0af0361e63158
Brian Chevalier <adambadahdah@gmail.com>
2021-12-07 22:56:04 -0700

    Update vega lite spec and add comments

--- src/portal/ui/viewer/vega_lite.cljs
+++ src/portal/ui/viewer/vega_lite.cljs
@@ -1,12 +1,17 @@
 (ns portal.ui.viewer.vega-lite
+  "Viewer for the Vega-Lite specification
+  https://vega.github.io/vega-lite/docs/spec.html"
   (:require [clojure.spec.alpha :as sp]
             [portal.ui.viewer.vega :as vega]))
 
+(sp/def ::name string?)
+(sp/def ::description string?)
 (sp/def ::$schema
   (sp/and string? #(re-matches #"https://vega\.github\.io/schema/vega-lite/v\d\.json" %)))
 
 (sp/def ::vega-lite
-  (sp/keys :req-un [::$schema]))
+  (sp/keys :req-un [::data]
+           :opt-un [::name ::description ::$schema]))
 
 (defn vega-lite-viewer [value]
   ^{:key (hash value)}
