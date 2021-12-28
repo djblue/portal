75ca37c138e0cc53505d1991a540cb54b8795401
Chris Badahdah <a.s.badahdah@gmail.com>
2021-12-28 14:17:07 -0700

    Include time in build output

--- dev/tasks/tools.clj
+++ dev/tasks/tools.clj
@@ -2,19 +2,41 @@
   (:require [babashka.deps :as deps]
             [babashka.process :as p]
             [clojure.string :as str]
-            [io.aviso.ansi :as a]))
+            [io.aviso.ansi :as a])
+  (:import [java.time Duration]))
 
 (def ^:dynamic *cwd* nil)
+
+(defn- now [] (System/currentTimeMillis))
+
+(defn- format-millis [ms]
+  (let [duration (Duration/ofMillis ms)
+        h        (mod (.toHours duration) 24)
+        m        (mod (.toMinutes duration) 60)
+        s        (mod (.toSeconds duration) 60)
+        ms       (mod ms 1000)]
+    (str
+     (a/bold-blue "->")
+     " "
+     (a/bold-yellow
+      (str
+       (when-not (zero? h)
+         (str h " hours, "))
+       (when-not (zero? m)
+         (str h " minutes, "))
+       s "." ms " seconds")))))
 
 (defn sh [& args]
   (println (a/bold-blue "=>")
            (a/bold-green (name (first args)))
            (a/bold (str/join " " (map name (rest args)))))
   (let [opts {:inherit true :dir *cwd*}
+        start (now)
         ps (if-not (= (first args) :clojure)
              (p/process (map name args) opts)
              (deps/clojure (map name (rest args)) opts))]
-    (p/check ps))
+    (p/check ps)
+    (println (format-millis (- (now) start)) "\n"))
   nil)
 
 (def bb     (partial sh :bb))
