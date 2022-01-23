0cba51b6f354fa7e3c03778f46e2e7bace8bde59
Chris Badahdah <a.s.badahdah@gmail.com>
2022-01-23 07:58:51 -0700

    Publish intellij plugin

--- dev/tasks/deploy.clj
+++ dev/tasks/deploy.clj
@@ -2,11 +2,15 @@
   (:require [tasks.ci :refer [ci]]
             [tasks.info :refer [version]]
             [tasks.package :as pkg]
-            [tasks.tools :refer [*cwd* clj npx]]))
+            [tasks.tools :refer [*cwd* clj gradle npx]]))
 
 (defn- deploy-vscode []
   (binding [*cwd* "extension-vscode"]
     (npx :vsce :publish)))
+
+(defn- deploy-intellij []
+  (binding [*cwd* "extension-intellij"]
+    (gradle :publishPlugin)))
 
 (defn- deploy-clojars []
   (clj "-M:deploy" (str "./target/portal-" version ".jar")))
@@ -14,7 +18,8 @@
 (defn deploy []
   (pkg/all)
   (deploy-clojars)
-  (deploy-vscode))
+  (deploy-vscode)
+  (deploy-intellij))
 
 (defn all
   "Deploy all artifacts."
