2fd1af411289edb87625efafdc57ee1567d1e296
Chris Badahdah <a.s.badahdah@gmail.com>
2022-01-29 13:59:59 -0700

    Fix theme and history

--- src/portal/ui/state.cljs
+++ src/portal/ui/state.cljs
@@ -73,7 +73,7 @@
 
 (defn- send! [message] (@sender message))
 
-(def no-history [::previous-commands ::c/theme])
+(def no-history [::previous-commands ::c/theme :theme])
 
 (defn history-back [state]
   (when-let [previous-state (:portal/previous-state state)]
