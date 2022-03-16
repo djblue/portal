f3ad4a532a16f38d176b3c9b75a001abfe74d4b7
Chris Badahdah <a.s.badahdah@gmail.com>
2022-03-16 12:28:42 -0700

    Start bb socket repl for dev

--- dev/tasks/dev.clj
+++ dev/tasks/dev.clj
@@ -1,6 +1,24 @@
 (ns tasks.dev
-  (:require [tasks.build :refer [build]]
+  (:require [clojure.core.server :as server]
+            [clojure.java.io :as io]
+            [tasks.build :refer [build]]
             [tasks.tools :refer [clj]]))
+
+(defn- start-server [opts]
+  (let [server    (server/start-server opts)
+        port      (.getLocalPort server)
+        host      (-> server .getInetAddress .getCanonicalHostName)
+        port-file (io/file ".bb-repl")]
+    (.deleteOnExit port-file)
+    (spit port-file (pr-str {:host host :port port :runtime :bb}))
+    (printf "=> Babashka socket repl listening on %s:%s\n" host port)))
+
+(defn socket-repl []
+  (start-server
+   {:name          "bb"
+    :port          0
+    :accept        clojure.core.server/repl
+    :server-daemon false}))
 
 (defn dev
   "Start dev server."
@@ -9,4 +27,4 @@
   (clj "-M:dev:cider:cljs:shadow"
        :watch :pwa :client :vs-code :electron))
 
-(defn -main [] (dev))
+(defn -main [] (socket-repl) (dev))
