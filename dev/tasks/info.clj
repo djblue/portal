fb312462e894861db0d54bc94aac791b03564d4d
Chris Badahdah <a.s.badahdah@gmail.com>
2022-03-16 00:33:51 -0700

    Fix cljdoc build

--- dev/tasks/info.clj
+++ dev/tasks/info.clj
@@ -23,7 +23,8 @@
     (merge
      (:deps deps)
      (provided (get-in deps [:aliases :cider :extra-deps]))
-     (provided (get-in deps [:aliases :cljs :extra-deps])))))
+     (provided (get-in deps [:aliases :cljs :extra-deps]))
+     (provided (get-in deps [:aliases :shadow :extra-deps])))))
 
 (def options
   {:lib           'djblue/portal
