1461a9cd3940883e7e5b84ed4653469d21e1a09e
David Pham <davidpham87@gmail.com>
2022-03-30 22:43:44 +0200

    Docs: addition of rich comment and links for specification

--- src/portal/ui/viewer/plotly.cljs
+++ src/portal/ui/viewer/plotly.cljs
@@ -1,6 +1,8 @@
 (ns portal.ui.viewer.plotly
   "Viewer for plotly
-  https://github.com/plotly/plotly.js specification"
+  For more information, see:
+  https://github.com/plotly/plotly.js
+  https://github.com/plotly/react-plotly.js"
   (:require
    [clojure.spec.alpha :as sp]
    ["react-plotly.js$default" :as plotly]))
@@ -8,7 +10,7 @@
 (sp/def ::name string?)
 (sp/def ::plotly
   (sp/keys :req-un [::data]
-           :opt-un [::name ::layout ::config]))
+           :opt-un [::name ::layout ::config ::style]))
 
 (defn plotly-viewer [value]
   ^{:key (hash value)}
@@ -19,3 +21,11 @@
   {:predicate (partial sp/valid? ::plotly)
    :component plotly-viewer
    :name :portal.viewer/plotly})
+
+(comment
+  (tap> {:data [{:x [0 2] :y [0 3]}]
+         :style {:width 960 :height 560}})
+  (tap> {:data [{:values [0 2 3 4 5]
+                 :type :pie}]
+         :style {:width "100%" :height "100%"}})
+  )
