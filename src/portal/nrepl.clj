272b0feedd854da35618ad17dfb2b577a62ea56f
Chris Badahdah <a.s.badahdah@gmail.com>
2022-07-05 19:10:41 -0700

    Include stdio / test-report output in portal.nrepl

--- src/portal/nrepl.clj
+++ src/portal/nrepl.clj
@@ -1,5 +1,6 @@
 (ns ^:no-doc portal.nrepl
   (:require [clojure.datafy :as d]
+            [clojure.test :as test]
             [nrepl.middleware :refer [set-descriptor!]]
             [nrepl.middleware.caught :as caught]
             [nrepl.middleware.print :as print]
@@ -51,10 +52,18 @@
   (send [_this  msg]
     (transport/send transport msg)
     (when (seq (p/sessions))
+      (when-let [out (:out msg)]
+        (swap! (:stdio handler-msg) conj {:tag :out :val out}))
+      (when-let [err (:err msg)]
+        (swap! (:stdio handler-msg) conj {:tag :err :val err}))
       (when-let [result (get-result msg)]
         (-> result
             (merge
-             (select-keys handler-msg [:ns :file :column :line :code]))
+             (select-keys handler-msg [:ns :file :column :line :code])
+             (when-let [report (-> handler-msg :report deref not-empty)]
+               {:report report})
+             (when-let [stdio (-> handler-msg :stdio deref not-empty)]
+               {:stdio stdio}))
             (update :ns (fnil symbol 'user))
             (assoc :time     (Date.)
                    :ms       (quot (- (System/nanoTime) (:start handler-msg)) 1000000)
@@ -62,12 +71,28 @@
             p/submit)))
     transport))
 
-(defn wrap-portal [handler]
-  (fn [msg]
+(defn- wrap-portal* [handler msg]
+  (let [report         (atom [])
+        test-report    test/report
+        portal-report  (fn [value]
+                         (swap! report conj value)
+                         (test-report value))]
     (handler
      (cond-> msg
        (= (:op msg) "eval")
-       (update :transport ->PortalTransport (assoc msg :start (System/nanoTime)))))))
+       (-> (update :transport
+                   ->PortalTransport
+                   (assoc msg
+                          :report report
+                          :stdio  (atom [])
+                          :start  (System/nanoTime)))
+           (update :session
+                   (fn [session]
+                     (swap! session assoc
+                            #'test/report portal-report)
+                     session)))))))
+
+(defn wrap-portal [handler] (partial #'wrap-portal* handler))
 
 (defn- get-shadow-middleware []
   (try
