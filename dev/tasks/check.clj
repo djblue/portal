e8558d2747bd2e4443d1cf4c05a80494a8bb30f6
Chris Badahdah <a.s.badahdah@gmail.com>
2022-09-23 18:17:19 -0700

    Add cloc fn

--- dev/tasks/check.clj
+++ dev/tasks/check.clj
@@ -1,6 +1,17 @@
 (ns tasks.check
-  (:require [tasks.format :as fmt]
-            [tasks.tools :refer [clj gradle *cwd*]]))
+  (:require [portal.runtime.json :as json]
+            [tasks.format :as fmt]
+            [tasks.tools :refer [sh clj gradle *cwd*]]))
+
+(defn cloc []
+  (-> (sh :cloc "--json" :src :dev :test)
+      (with-out-str)
+      (json/read)
+      (dissoc :header)
+      (with-meta
+        {:portal.viewer/default :portal.viewer/table
+         :portal.viewer/table
+         {:columns [:nFiles :blank :comment :code]}})))
 
 (defn check
   "Run all static analysis checks."
