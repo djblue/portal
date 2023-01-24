ca1db1e0e1a0b42d6652f1b75f9f76c8336a099d
Chris Badahdah <a.s.badahdah@gmail.com>
2023-01-23 22:09:20 -0700

    Forward stderr / stdout for `portal.api/eval-str`

--- src/portal/ui/sci.cljs
+++ src/portal/ui/sci.cljs
@@ -14,24 +14,32 @@
                         (catch :default _
                           (sci/create-ns ns nil)))
                    (sci/create-ns 'cljs.user nil))
-          reader (sci/reader (:code msg))]
+          reader (sci/reader (:code msg))
+          stdio  (atom [])
+          out-fn (fn [val] (swap! stdio conj val))]
       (sci/with-bindings
         {sci/*1 *1
          sci/*2 *2
          sci/*3 *3
          sci/*e *e
          sci/ns sci-ns
+         sci/print-newline true
+         sci/print-fn      #(out-fn {:tag :out :val %})
+         sci/print-err-fn  #(out-fn {:tag :err :val %})
          sci/file (:file msg)}
-        {:value (loop [last-val nil]
-                  (let [form (sci/parse-next ctx reader)]
-                    (if (= ::sci/eof form)
-                      last-val
-                      (let [value (sci/eval-form ctx form)]
-                        (set! *3 *2)
-                        (set! *2 *1)
-                        (set! *1 value)
-                        (recur value)))))
-         :ns    (str @sci/ns)}))
+        (cond->
+         {:value (loop [last-val nil]
+                   (let [form (sci/parse-next ctx reader)]
+                     (if (= ::sci/eof form)
+                       last-val
+                       (let [value (sci/eval-form ctx form)]
+                         (set! *3 *2)
+                         (set! *2 *1)
+                         (set! *1 value)
+                         (recur value)))))
+          :ns    (str @sci/ns)}
+
+          (seq @stdio) (assoc :stdio @stdio))))
     (catch :default e
       (let [sci-error? (isa? (:type (ex-data e)) :sci/error)]
         (throw (if sci-error?
