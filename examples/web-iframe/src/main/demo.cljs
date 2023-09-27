fee7bbe31e92efccbd75307d7c1d2fb7919a9fd1
Felipe Cortez <github@felipecortez.net>
2023-09-27 09:48:29 -0300

    Unqualify iframe-parent launcher option
    + move iframe append logic to launcher

--- examples/web-iframe/src/main/demo.cljs
+++ examples/web-iframe/src/main/demo.cljs
@@ -4,18 +4,13 @@
    [portal.web :as pw]))
 
 (defn- app []
-  [:<>
+  [:div {:style {:display :flex, :height "100vh", :flex-direction "column"}}
    [:div {:style {:display :flex, :justify-content :center}}
     [:button {:on-click #(pw/submit {:hello :world, :n (rand-int 10)})
               :style {:padding 8, :margin 8, :width "100%"}}
      "Test"]]
-   [:div
-    {:ref
-     (fn [x]
-       (when x
-         (let [iframe (doto (pw/open {:portal.launcher/iframe-parent x})
-                        (.setAttribute "style" "width: 100vw; height: 90vh; border: 0"))]
-           (.appendChild x iframe))))}]])
+   [:div {:style {:flex-grow 1}
+          :ref (fn [el] (when el (pw/open {:iframe-parent el})))}]])
 
 (defn ^:dev/after-load init []
   (rdom/render [app] (js/document.getElementById "root")))
