36f861059630faeac4676356018a5369bf463b2f
Chris Badahdah <a.s.badahdah@gmail.com>
2023-12-04 09:56:49 -0700

    Use state hook for deref viewer

--- src/portal/ui/viewer/deref.cljs
+++ src/portal/ui/viewer/deref.cljs
@@ -1,65 +1,65 @@
 (ns ^:no-doc portal.ui.viewer.deref
-  (:require [portal.colors :as c]
+  (:require ["react" :as react]
+            [portal.colors :as c]
             [portal.ui.icons :as icons]
             [portal.ui.inspector :as ins]
             [portal.ui.options :as options]
             [portal.ui.rpc :as rpc]
             [portal.ui.select :as select]
             [portal.ui.styled :as d]
-            [portal.ui.theme :as theme]
-            [reagent.core :as r]))
+            [portal.ui.theme :as theme]))
 
 (defn atom? [value]
   (and (satisfies? cljs.core/IDeref value)
        (not (instance? cljs.core/Var value))))
 
 (defn- toggle-watch [value]
-  (r/with-let [hover (r/atom false)]
-    (let [theme  (theme/use-theme)
-          opts   (options/use-options)
-          value' @value
-          deref? (some-> opts :watch-registry deref (contains? value))]
-      [d/div
-       {:style
-        {:top        0
-         :right      0
-         :z-index    100
-         :opacity    0.5
-         :display    :flex
-         :gap        (:padding theme)
-         :position   :absolute
-         :cursor     :pointer
-         :color      (if deref?
-                       (::c/tag theme)
-                       (::c/exception theme))
-         :font-size  (:font-size theme)
-         :box-sizing :border-box
-         :padding    (if-not (coll? value')
-                       0
-                       (inc (:padding theme)))}
+  (let [theme  (theme/use-theme)
+        opts   (options/use-options)
+        value' @value
+        deref? (some-> opts :watch-registry deref (contains? value))
+        [hover set-hover!] (react/useState false)]
+    [d/div
+     {:style
+      {:top        0
+       :right      0
+       :z-index    100
+       :opacity    0.5
+       :display    :flex
+       :gap        (:padding theme)
+       :position   :absolute
+       :cursor     :pointer
+       :color      (if deref?
+                     (::c/tag theme)
+                     (::c/exception theme))
+       :font-size  (:font-size theme)
+       :box-sizing :border-box
+       :padding    (if-not (coll? value')
+                     0
+                     (inc (:padding theme)))}
 
-        :style/hover {:opacity 1}
+      :style/hover {:opacity 1}
 
-        :on-mouse-enter (fn [_e] (reset! hover true))
-        :on-mouse-leave (fn [_e] (reset! hover false))}
-       (when @hover
-         (if deref?
-           [icons/pause
-            {:title "Pause watch."
-             :on-click
-             (fn [e]
-               (.stopPropagation e)
-               (rpc/call 'portal.api/toggle-watch value))}]
-           [icons/play
-            {:title "Resume watch."
-             :on-click
-             (fn [e]
-               (.stopPropagation e)
-               (rpc/call 'portal.api/toggle-watch value))}]))
-       [icons/at
-        {:title (str "Click to select atom. "
-                     (when-not deref?
-                       "(watch paused)"))}]])))
+      :on-mouse-enter (fn [_e] (set-hover! true))
+      :on-mouse-leave (fn [_e] (set-hover! false))}
+     (when hover
+       (if deref?
+         [icons/pause
+          {:title "Pause watch."
+           :on-click
+           (fn [e]
+             (.stopPropagation e)
+             (rpc/call 'portal.api/toggle-watch value))}]
+         [icons/play
+          {:title "Resume watch."
+           :on-click
+           (fn [e]
+             (.stopPropagation e)
+             (rpc/call 'portal.api/toggle-watch value))}]))
+     [icons/at
+      {:title (str "Click to select atom. "
+                   (when-not deref?
+                     "(watch paused)"))}]]))
 
 (defn inspect-deref [value]
   [d/div
