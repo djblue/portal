0f297d20d6a897315f6e35c615cd5ab9f027a268
Chris Badahdah <a.s.badahdah@gmail.com>
2023-12-16 17:54:57 -0700

    Allow selecting platform version for `bb ide`

--- dev/tasks/ide.clj
+++ dev/tasks/ide.clj
@@ -1,8 +1,17 @@
 (ns tasks.ide
-  (:require [tasks.tools :refer [*cwd* gradle]]))
+  (:require [clojure.string :as str]
+            [tasks.tools :refer [*cwd* gradle]]))
+
+(def plugins
+  {"2021.3" ["com.cursiveclojure.cursive:1.12.2-2021.3" "com.intellij.java"]
+   "2022.3" ["com.cursiveclojure.cursive:1.13.1-2022.3" "com.intellij.java"]
+   "2023.3" ["com.cursiveclojure.cursive:1.13.1-2023.3" "com.intellij.java"]})
 
 (defn open
   "Open dev extension for intellij."
-  []
+  [& [version]]
   (binding [*cwd* "extension-intellij"]
-    (gradle "runIde")))
+    (let [platform (or version "2023.3")]
+      (gradle "runIde"
+              (str "-PplatformVersion=" platform)
+              (str "-PplatformPlugins=" (str/join ", " (get plugins platform)))))))
