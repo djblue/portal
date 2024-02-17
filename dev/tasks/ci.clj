0f2e316cc1de990e0e558a89da86ce0aff3080b7
Chris Badahdah <a.s.badahdah@gmail.com>
2024-02-17 14:57:09 -0700

    Re-work github actions for deploy (#212)

--- dev/tasks/ci.clj
+++ dev/tasks/ci.clj
@@ -1,6 +1,20 @@
 (ns tasks.ci
   (:require [tasks.check :refer [check check*]]
-            [tasks.test :refer [test test*]]))
+            [tasks.test :refer [test test*] :as test]
+            [tasks.tools :refer [clj]]))
+
+(def ^:private commands
+  ["-M:cljfmt"
+   "-M:cljs"
+   "-M:cljs:shadow"
+   "-M:dev"
+   "-M:kondo"
+   "-M:test"
+   "-X:deploy"])
+
+(defn setup []
+  (test/setup)
+  (doseq [command commands] (clj "-Sforce" "-Spath" command)))
 
 (defn ci
   "Run all CI Checks."
