djblue/portal

commit dc33869fef516195290c2cc35e901bbf07208fb0

Improve support for native java interfaces

src/portal/runtime.cljc
@@ -231,6 +231,31 @@
          m   (assoc :meta m)
          rep (assoc :rep rep))))))
 
+#?(:bb nil
+   :clj
+   (extend-type java.util.Collection
+     cson/ToJson
+     (-to-json [value buffer]
+       (cson/tagged-coll
+        buffer
+        (cond
+          (instance? java.util.Set value)          "#"
+          (instance? java.util.RandomAccess value) "["
+          :else                                    "(")
+        {::id (value->id value) ::type (type value)}
+        value))))
+
+#?(:bb nil
+   :clj
+   (extend-type java.util.Map
+     cson/ToJson
+     (-to-json [value buffer]
+       (cson/tagged-map
+        buffer
+        "{"
+        {::id (value->id value) ::type (type value)}
+        value))))
+
 (extend-type #?(:clj  Object
                 :cljr Object
                 :cljs default)

src/examples/data.cljc
@@ -67,6 +67,19 @@
       ::js-array #js [0 1 2 3 4]
       ::js-object #js {:hello "world"}}))
 
+(def platform-collections
+  #?(:bb nil
+     :clj
+     {:java.util/ArrayList (java.util.ArrayList.)
+      :java.util/HashMap (java.util.HashMap.)
+      :java.util/HashSet (java.util.HashSet.)
+      :java.util/LinkedHashSet (java.util.LinkedHashSet.)
+      :java.util/LinkedList (java.util.LinkedList.)
+      :java.util/Stack (java.util.Stack.)
+      :java.util/TreeSet (java.util.TreeSet.)
+      :java.util/Vector (java.util.Vector.)}
+     :default nil))
+
 (def basic-data
   {::booleans #{true false}
    ::nil nil
@@ -1102,21 +1115,24 @@
    :portal.viewer/spec))
 
 (def data
-  {::platform-data      platform-data
-   ::hacker-news        #?(:org.babashka/nbb nil :default hn/stories)
-   ::spec-data          spec-data
-   ::table-data         table-data
-   ::diff               diff-data
-   ::basic-data         basic-data
-   ::themes             c/themes
-   ::clojure-data       clojure-data
-   ::hiccup             hiccup
-   ::data-visualization data-visualization
-   ::string-data        string-data
-   ::log-data           log-data
-   ::profile-data       profile-data
-   ::test-data          test-report
-   ::prepl-data         prepl-data
-   ::exception-data     exception-data
-   ::http-requests      http-requests
-   ::http-responses     http-responses})
+  (merge
+   {::platform-data      platform-data
+    ::hacker-news        #?(:org.babashka/nbb nil :default hn/stories)
+    ::spec-data          spec-data
+    ::table-data         table-data
+    ::diff               diff-data
+    ::basic-data         basic-data
+    ::themes             c/themes
+    ::clojure-data       clojure-data
+    ::hiccup             hiccup
+    ::data-visualization data-visualization
+    ::string-data        string-data
+    ::log-data           log-data
+    ::profile-data       profile-data
+    ::test-data          test-report
+    ::prepl-data         prepl-data
+    ::exception-data     exception-data
+    ::http-requests      http-requests
+    ::http-responses     http-responses}
+   (when platform-data
+     {::platform-collections platform-collections})))

src/portal/runtime/cson.cljc
@@ -203,10 +203,13 @@
   (let [m (->value buffer) v (->value buffer)]
     (if-not (can-meta? v) v (with-meta v m))))
 
-(defn- tagged-meta [buffer value]
-  (if-let [m (meta value)]
+(defn- push-meta [buffer m]
+  (if (map? m)
     (-to-json m (json/push-string buffer "^"))
     buffer))
+
+(defn- tagged-meta [buffer value]
+  (push-meta buffer (meta value)))
 
 (defn- bb-fix
   "Remove bb type tag for records which cause an infinite loop."
@@ -450,14 +453,17 @@
 (defn- ->symbol-2 [buffer]
   (symbol (json/next-string buffer) (json/next-string buffer)))
 
-(defn- tagged-coll [buffer tag value]
-  (reduce
-   to-json
-   (-> buffer
-       (tagged-meta value)
-       (json/push-string tag)
-       (json/push-long (count value)))
-   value))
+(defn tagged-coll
+  ([buffer tag value]
+   (tagged-coll buffer tag (meta value) value))
+  ([buffer tag meta-map  value]
+   (reduce
+    to-json
+    (-> buffer
+        (push-meta meta-map)
+        (json/push-string tag)
+        (json/push-long (count value)))
+    value)))
 
 #?(:bb (def clojure.lang.APersistentMap$KeySeq (type (keys {0 0}))))
 #?(:bb (def clojure.lang.APersistentMap$ValSeq (type (vals {0 0}))))
@@ -664,16 +670,19 @@
         (compare (get order a) (get order b))))
      values)))
 
-(defn- tagged-map
-  ([buffer value] (tagged-map buffer "{" value))
+(defn tagged-map
+  ([buffer value]
+   (tagged-map buffer "{" value))
   ([buffer tag value]
+   (tagged-map buffer tag (meta value) value))
+  ([buffer tag meta-map  value]
    (reduce-kv
     (fn [buffer k v]
       (-> buffer
           (to-json k)
           (to-json v)))
     (-> buffer
-        (tagged-meta value)
+        (push-meta meta-map)
         (json/push-string tag)
         (json/push-long (count value)))
     value)))

commit 5cfd6f2835100db7c83ab581261f46ab4bf36179

Prevent issue with records and serialization
Fixes #236
Issue caused by dc33869

src/portal/runtime.cljc
@@ -257,7 +257,9 @@
          (cson/tagged-map
           buffer
           "{"
-          {::id (value->id value) ::type (type value)}
+          (if (record? value)
+            (meta value)
+            {::id (value->id value) ::type (type value)})
           value)))))
 
 (extend-type #?(:clj  Object