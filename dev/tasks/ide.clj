ad9bc3e7213833fa167d4bb05ba38cf0b602c897
Chris Badahdah <a.s.badahdah@gmail.com>
2024-12-19 20:31:29 -0700

    Update `bb ide` to latest intellij and cursive versions

--- dev/tasks/ide.clj
+++ dev/tasks/ide.clj
@@ -5,13 +5,14 @@
 (def plugins
   {"2021.3" ["com.cursiveclojure.cursive:1.12.2-2021.3" "com.intellij.java"]
    "2022.3" ["com.cursiveclojure.cursive:1.13.1-2022.3" "com.intellij.java"]
-   "2023.3" ["com.cursiveclojure.cursive:1.13.1-2023.3" "com.intellij.java"]})
+   "2023.3" ["com.cursiveclojure.cursive:1.13.1-2023.3" "com.intellij.java"]
+   "2024.3" ["com.cursiveclojure.cursive:1.14.0-2024.3" "com.intellij.java"]})
 
 (defn open
   "Open dev extension for intellij."
   [& [version]]
   (binding [*cwd* "extension-intellij"]
-    (let [platform (or version "2023.3")]
+    (let [platform (or version "2024.3")]
       (gradle "runIde"
               (str "-PplatformVersion=" platform)
               (str "-PplatformPlugins=" (str/join ", " (get plugins platform)))))))
