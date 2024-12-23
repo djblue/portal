djblue/portal

commit 73e00704eeb84cf6bc4c7417de92a4925e1c47a3

Improve launcher support on windows

src/portal/runtime/browser.cljc
@@ -77,17 +77,20 @@
   (str/trim (:out (shell/sh "cmd.exe" "/C" "echo %USERNAME%"))))
 
 (defn- windows-chrome-web-applications []
-  (tree-seq
-   (fn [f]
-     (and (not (fs/is-file f))
-          (or
-           (str/includes? f "_crx_")
-           (str/ends-with? f "Web Applications"))))
-   (fn [d] (fs/list d))
-   (fs/join
-    "/mnt/c/Users"
-    (get-windows-user)
-    "AppData/Local/Google/Chrome/User Data/Default/Web Applications")))
+  (some
+   (fn [root]
+     (tree-seq
+      (fn [f]
+        (and (not (fs/is-file f))
+             (or
+              (str/includes? f "_crx_")
+              (str/ends-with? f "Web Applications"))))
+      (fn [d] (fs/list d))
+      (fs/join
+       root
+       (get-windows-user)
+       "AppData/Local/Google/Chrome/User Data/Default/Web Applications")))
+   ["/Users" "/mnt/c/Users"]))
 
 (defn- get-app-id-profile-windows [app-name]
   (try

commit cdabd7fd73a0cb8cb05a2dd6f790a9770643aaca

Fix WSL regression caused by 73e0070

src/portal/runtime/browser.cljc
@@ -77,7 +77,7 @@
   (str/trim (:out (shell/sh "cmd.exe" "/C" "echo %USERNAME%"))))
 
 (defn- windows-chrome-web-applications []
-  (some
+  (mapcat
    (fn [root]
      (tree-seq
       (fn [f]
@@ -85,12 +85,15 @@
              (or
               (str/includes? f "_crx_")
               (str/ends-with? f "Web Applications"))))
-      (fn [d] (fs/list d))
+      (fn [d]
+        (try
+          (fs/list d)
+          (catch #?(:cljs :default :default Exception) _)))
       (fs/join
        root
        (get-windows-user)
        "AppData/Local/Google/Chrome/User Data/Default/Web Applications")))
-   ["/Users" "/mnt/c/Users"]))
+   ["/mnt/c/Users" "/Users"]))
 
 (defn- get-app-id-profile-windows [app-name]
   (try