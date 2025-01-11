2c53ff745611a3a1909d5758b6e60d9bb28c4106
Chris Badahdah <a.s.badahdah@gmail.com>
2025-01-11 13:22:13 -0700

    Update CLR to support editor extensions

--- src/portal/runtime/clr/launcher.clj
+++ src/portal/runtime/clr/launcher.clj
@@ -1,11 +1,129 @@
 (ns ^:no-doc ^:no-check portal.runtime.clr.launcher
-  (:require [clojure.string :as str]
+  (:require [clojure.edn :as edn]
+            [clojure.string :as str]
             [portal.runtime :as rt]
             [portal.runtime.browser :as browser]
             [portal.runtime.clr.client :as c]
-            [portal.runtime.clr.server :as server])
-  (:import (System.Net HttpListener HttpListenerContext)
+            [portal.runtime.clr.server :as server]
+            [portal.runtime.fs :as fs]
+            [portal.runtime.shell :refer [spawn]])
+  (:import (System.Collections.Generic KeyValuePair)
+           (System.Net HttpListener HttpListenerContext)
+           (System.Net.Http
+            HttpClient
+            HttpCompletionOption
+            HttpMethod
+            HttpRequestMessage
+            HttpResponseMessage
+            StringContent)
+           (System.Net.Http.Headers HttpResponseHeaders)
            (System.Text Encoding)))
+
+(defn- get-search-paths []
+  (->> (fs/cwd) (iterate fs/dirname) (take-while some?)))
+
+(defn- get-config [{:keys [options config-file]}]
+  (let [search-paths (get-search-paths)]
+    (or (some
+         (fn [parent]
+           (some-> parent
+                   (fs/join ".portal" config-file)
+                   fs/exists
+                   fs/slurp
+                   edn/read-string
+                   (merge (when-let [config (:launcher-config options)]
+                            config))))
+         search-paths)
+        (throw
+         (ex-info
+          (str "No config file found: " config-file)
+          {:options      options
+           :config-file  config-file
+           :search-paths search-paths})))))
+
+(def ^:private client (HttpClient.))
+
+(defn- ->resposne-headers [^HttpResponseMessage response]
+  (let [^HttpResponseHeaders headers         (.Headers response)
+        ^HttpResponseHeaders content-headers (.Headers (.Content response))]
+    (reduce
+     (fn [out ^KeyValuePair x]
+       (let [k (.Key x) v (.Value x)]
+         (assoc out
+                (keyword (str/lower-case k))
+                (cond-> v (= 1 (count v)) first))))
+     {}
+     (concat
+      (iterator-seq (.GetEnumerator headers))
+      (iterator-seq (.GetEnumerator content-headers))))))
+
+(defn- ->response [^HttpResponseMessage response]
+  {:status  (int (.StatusCode response))
+   :headers (->resposne-headers response)
+   :body    (.Content response)})
+
+(defn- fetch [url
+              {:keys [method body encoding]
+               :or   {encoding :edn
+                      method :get}}]
+  (let [request  (HttpRequestMessage.
+                  (case method
+                    :get HttpMethod/Get
+                    :post HttpMethod/Post
+                    :put HttpMethod/Put
+                    :delete HttpMethod/Delete)
+                  url)]
+    (when body
+      (set! (.-Content request)
+            (StringContent.
+             body
+             Encoding/UTF8
+             (case encoding
+               :edn "application/edn"
+               :cson "application/cson"))))
+    (->response (.Send ^HttpClient client request HttpCompletionOption/ResponseContentRead))))
+
+(defn- remote-open [{:keys [portal options server] :as args}]
+  (let [config (get-config args)
+        url  (str "http://" (:host config) ":" (:port config) "/open")
+        opts {:method :post
+              :body   (pr-str {:portal  (into {} portal)
+                               :options (select-keys options [:window-title])
+                               :server  (select-keys server [:host :port])})}
+        {:keys [status error] :as response} (fetch url opts)]
+    (when (or error (not= status 200))
+      (throw (ex-info "Unable to open extension"
+                      {:options  options
+                       :config   config
+                       :response (select-keys response [:body :headers :status])}
+                      error)))))
+
+(defmethod browser/-open :intellij [args]
+  (try
+    (remote-open (assoc args :config-file "intellij.edn"))
+    (catch Exception e
+      (throw
+       (ex-info
+        (str
+         (ex-message e)
+         ": Please ensure extension is installed and Portal tab is open.")
+        (ex-data e))))))
+
+(defmethod browser/-open :vs-code  [args] (remote-open (assoc args :config-file "vs-code.edn")))
+
+(defmethod browser/-open :emacs [{:keys [portal server]}]
+  (let [url (str "http://" (:host server) ":" (:port server) "?" (:session-id portal))]
+    (spawn "emacsclient" "--no-wait" "--eval"
+           (str "(xwidget-webkit-browse-url " (pr-str url) ")"))))
+
+(defmethod browser/-open :electron [args] (remote-open (assoc args :config-file "electron.edn")))
+
+(defmethod browser/-open :auto [args]
+  (browser/-open
+   (assoc-in args [:options :launcher]
+             (cond
+               (fs/exists ".portal/vs-code.edn") :vs-code
+               (fs/exists ".portal/intellij.edn") :intellij))))
 
 (defn- read-request [^HttpListenerContext context]
   (let [request            (.Request context)
@@ -71,8 +189,8 @@
             :host host})))))
 
 (defn stop []
-  (when-let [server @server]
-    (.Stop ^HttpListener (:http-server server)))
+  (when-let [^HttpListener server (:http-server @server)]
+    (.Stop server))
   (reset! server nil))
 
 (defn open
