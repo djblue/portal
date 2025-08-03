07ac80ce4aa4f93b1c275967c56ee37795b04967
Chris Badahdah <a.s.badahdah@gmail.com>
2025-08-03 13:09:38 -0700

    Cleanup `tasks.bench`

--- dev/tasks/bench.clj
+++ dev/tasks/bench.clj
@@ -24,12 +24,28 @@
   {{:encoding :cson :test :read}
    {:launcher :auto :window-title "cson-read"}
    {:encoding :cson :test :write}
-   {:launcher :auto :window-title "cson-write"}})
+   {:launcher :auto :window-title "cson-write"}
+   {:runtime :clj :test :read}
+   {:launcher :auto :window-title "clj-read"}
+   {:runtime :clj :test :write}
+   {:launcher :auto :window-title "clj-write"}
+   {:runtime :cljs :test :read}
+   {:launcher :auto :window-title "cljs-read"}
+   {:runtime :cljs :test :write}
+   {:launcher :auto :window-title "cljs-write"}})
 
 (defn charts [data]
+  #_(p/inspect (bc/charts data) {:launcher :auto :window-title "cson-all"})
+  #_(let [groups (group-by #(select-keys % [:runtime :test]) data)]
+      (doseq [[group opts] windows
+              :let [sub-data (get groups group)]
+              :when sub-data]
+        (p/inspect (bc/charts sub-data) opts)))
   (let [groups (group-by #(select-keys % [:encoding :test]) data)]
-    (doseq [[group opts] windows]
-      (p/inspect (bc/charts (get groups group groups)) opts))))
+    (doseq [[group opts] windows
+            :let [sub-data (get groups group)]
+            :when sub-data]
+      (p/inspect (bc/charts sub-data) opts))))
 
 (defn -main
   "Run cson benchmarks and render results in portal"
