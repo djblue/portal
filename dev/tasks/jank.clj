b29400246f25c7a087cfb2b5999fb89dff4a0744
Chris Badahdah <contact@djblue.dev>
2026-06-21 11:46:36 -0700

    Cleanup

--- dev/tasks/jank.clj
+++ dev/tasks/jank.clj
@@ -1,13 +1,8 @@
 (ns tasks.jank
   (:require
-   [portal.api :as p]
-   [tasks.tools :refer [*opts* jank]]))
+   [tasks.tools :refer [jank]]))
 
 (defn -main
   "Start jank dev env / nrepl"
   []
-  (let [server (p/start {})]
-    (.addShutdownHook (Runtime/getRuntime) (Thread. (fn [] (p/close))))
-    (p/open {:launcher :auto})
-    (binding [*opts* {:extra-env {"PORTAL_PORT" (:port server)}}]
-      (jank :repl))))
+  (jank :repl))
